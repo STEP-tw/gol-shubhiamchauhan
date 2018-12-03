2157544a7ad66bb51bd4ed1076ea704e4728593e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-03 05:13:00 +0000

    Deploy STEP-tw/gol-shubhiamchauhan to github.com/STEP-tw/gol-shubhiamchauhan.git:gh-pages

---
+++ precache-manifest.81fa06a92e582ed58c9bba84e00de97c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e4a1ddb624c015e58f0",
+    "url": "/gol-shubhiamchauhan/static/js/runtime~main.7e4a1ddb.js"
+  },
+  {
+    "revision": "f978524a8c57bd44fef5",
+    "url": "/gol-shubhiamchauhan/static/js/main.f978524a.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/gol-shubhiamchauhan/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "f978524a8c57bd44fef5",
+    "url": "/gol-shubhiamchauhan/static/css/main.31379033.chunk.css"
+  },
+  {
+    "revision": "befc0b115302b8012f399da9ff226ec9",
+    "url": "/gol-shubhiamchauhan/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/gol-shubhiamchauhan/precache-manifest.81fa06a92e582ed58c9bba84e00de97c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/gol-shubhiamchauhan/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
